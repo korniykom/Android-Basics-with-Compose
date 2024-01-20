f1701fc37870c56d0c3d6e8de452a3aa58b49235
korniykom <korniykom@gmail.com>
2024-01-20 13:52:34 +0200

    updated font size to 18.sp

--- Unit2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Unit2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -89,7 +90,8 @@ fun ImageAndText(modifier: Modifier = Modifier) {
             modifier = modifier.height(16.dp)
         )
         Text(
-            text = stringResource(id = textResource)
+            text = stringResource(id = textResource),
+            fontSize = 18.sp
         )
     }
 }

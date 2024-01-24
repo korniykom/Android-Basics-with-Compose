28e5dfd85104e312d9f8dbfd05e5976ba6092b20
Max <korniykom@gmail.com>
2024-01-24 13:30:52 +0200

    Grid App

--- Unit3/Grid/app/src/main/java/com/example/grid/MainActivity.kt
+++ Unit3/Grid/app/src/main/java/com/example/grid/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.grid
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material3.Card
@@ -24,6 +29,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.grid.data.DataSource
+import com.example.grid.model.Topic
 import com.example.grid.ui.theme.GridTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,8 +43,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GridCard()
-
+                    AppGrid()
                 }
             }
         }
@@ -46,7 +52,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GridCard(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    gridItem: Topic
 ) {
     Card(
         modifier = modifier
@@ -54,23 +61,32 @@ fun GridCard(
     ) {
         Row {
             Image(
-                painter = painterResource(id = R.drawable.architecture),
+                painter = painterResource(id = gridItem.photo),
                 contentDescription = null,
                 modifier
                     .height(68.dp)
 
             )
             Column {
-                Text(text = stringResource(
-                    id = R.string.architecture),
+                Text(
+                    text = stringResource(
+                    id = gridItem.name),
                     modifier
                         .padding(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 8.dp)
                 )
                 Row(modifier = modifier
                     .padding(start = 16.dp, end = 16.dp)
                     .align(Alignment.CenterHorizontally)) {
-                    Icon(Icons.Rounded.Check, contentDescription = null )
-                    Text(text = "321")
+                    Icon(
+                        Icons.Rounded.Check,
+                        contentDescription = null,
+                        modifier = Modifier.height(16.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = gridItem.description.toString(),
+                        style = MaterialTheme.typography.labelMedium
+                    )
                 }
 
             }
@@ -78,8 +94,27 @@ fun GridCard(
     }
 }
 
+@Composable
+fun Grid(gridList: List<Topic>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 200.dp) ,
+        content = {
+            items(gridList) {
+                item -> GridCard(
+                    modifier = Modifier.padding(8.dp),
+                    gridItem = item
+                )
+            }
+        } )
+}
+
+@Composable
+fun AppGrid() {
+    Grid(gridList = DataSource().loadTopics())
+}
+
 @Preview
 @Composable
 fun PreviewCard() {
-    GridCard()
+    AppGrid()
 }

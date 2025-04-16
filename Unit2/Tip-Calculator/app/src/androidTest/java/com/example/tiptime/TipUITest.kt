de6f7089473b7999770bf98ff549cc647b0bfaa3
Max <korniykom@gmail.com>
2025-04-16 15:48:23 +0300

    chore: update agp to 8.9.1 in TipCalculator

--- Unit2/Tip-Calculator/app/src/androidTest/java/com/example/tiptime/TipUITest.kt
+++ Unit2/Tip-Calculator/app/src/androidTest/java/com/example/tiptime/TipUITest.kt
@@ -1,9 +1,5 @@
 package com.example.tiptime
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTextInput
@@ -13,24 +9,26 @@ import org.junit.Test
 import java.text.NumberFormat
 
 class TipUITest {
-    @get:Rule
-    val composeTestRule = createComposeRule()
+    class TipUITest {
+        @get:Rule
+        val composeTestRule = createComposeRule()
 
-    @Test
-    fun calculate_20_percent_tip() {
-        composeTestRule.setContent() {
-            TipTimeTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                TipTimeLayout()
+        @Test
+        fun calculate_20_percent_tip() {
+            composeTestRule.setContent {
+                TipTimeTheme {
+                    TipTimeLayout()
                 }
             }
+            composeTestRule.onNodeWithText("Bill Amount").performTextInput("10")
+
+            composeTestRule.onNodeWithText("Tip Percentage").performTextInput("20")
+            val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+            composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+                "No node with this text was found."
+            )
+
         }
-        composeTestRule.onNodeWithText("Bill Amount")
-            .performTextInput("10")
-        composeTestRule.onNodeWithText("Tip Percentage")
-            .performTextInput("20")
-        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
-        composeTestRule.onNodeWithText("Tip Amount: $expectedTip")
-            .assertExists("No node with this text was found.")
+
     }
 }
